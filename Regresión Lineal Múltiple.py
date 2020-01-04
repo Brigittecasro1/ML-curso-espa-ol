6ebf83aea84811e29c5c3aa923396ab17fe6636d
Brigittecasro1 <39300394+Brigittecasro1@users.noreply.github.com>
2020-01-04 20:13:55 +0400

    Create Regresión Lineal Múltiple.py

---
+++ Regresión Lineal Múltiple.py
@@ -0,0 +1,112 @@
+# Regresión Lineal Múltiple
+
+# Cómo importar las librerías
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importar el data set
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+
+# Codificar datos categóricos
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder  
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Evitar la trampa de las variables ficticias
+X = X[:, 1:]
+
+# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Ajustar el modelo de Regresión lineal múltiple con el conjunto de entrenamiento
+from sklearn.linear_model import LinearRegression
+regression = LinearRegression()
+regression.fit(X_train, y_train)
+
+# Predicción de los resultados en el conjunto de testing
+y_pred = regression.predict(X_test)
+
+# Construir el modelo óptimo de RLM utilizando la Eliminación hacia atrás
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+SL = 0.05
+
+#Se ha añadido el modificador .tolist() al X_opt para adaptarse a Python 3.7
+
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regression_OLS = sm.OLS(endog = y, exog = X_opt.tolist()).fit()
+regression_OLS.summary()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regression_OLS = sm.OLS(endog = y, exog = X_opt.tolist()).fit()
+regression_OLS.summary()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regression_OLS = sm.OLS(endog = y, exog = X_opt.tolist()).fit()
+regression_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+regression_OLS = sm.OLS(endog = y, exog = X_opt.tolist()).fit()
+regression_OLS.summary()
+
+X_opt = X[:, [0, 3]]
+regression_OLS = sm.OLS(endog = y, exog = X_opt.tolist()).fit()
+regression_OLS.summary()
+
+
+##Eliminación hacia atrás utilizando solamente p-valores:
+import statsmodels.api as sm
+def backwardElimination(x, sl):    
+    numVars = len(x[0])    
+    for i in range(0, numVars):   
+        regressor_OLS = sm.OLS(y, x.tolist()).fit()  
+        maxVar = max(regressor_OLS.pvalues).astype(float)        
+        if maxVar > sl:            
+            for j in range(0, numVars - i):                
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
+                    x = np.delete(x, j, 1)    
+    regressor_OLS.summary()    
+    return x 
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+
+
+##Eliminación hacia atrás utilizando  p-valores y el valor de  R Cuadrado Ajustado:
+import statsmodels.api as sm
+def backwardElimination(x, SL):    
+    numVars = len(x[0])    
+    temp = np.zeros((50,6)).astype(int)    
+    for i in range(0, numVars):        
+        regressor_OLS = sm.OLS(y, x.tolist()).fit()        
+        maxVar = max(regressor_OLS.pvalues).astype(float)        
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)        
+        if maxVar > SL:            
+            for j in range(0, numVars - i):                
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
+                    temp[:,j] = x[:, j]                    
+                    x = np.delete(x, j, 1)                    
+                    tmp_regressor = sm.OLS(y, x.tolist()).fit()                    
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)                    
+                    if (adjR_before >= adjR_after):                        
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))                        
+                        x_rollback = np.delete(x_rollback, j, 1)     
+                        print (regressor_OLS.summary())                        
+                        return x_rollback                    
+                    else:                        
+                        continue    
+    regressor_OLS.summary()    
+    return x 
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
